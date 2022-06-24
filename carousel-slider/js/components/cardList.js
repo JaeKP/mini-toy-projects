20b6699c193fe09e24420531f276e98f85a950a8
JK <worudp12@gmail.com>
2022-06-24 16:39:56 +0900

    #8 refac: querySelevtor를 data객체의 paretNode 속성으로 변경

--- carousel-slider/js/components/cardList.js
+++ carousel-slider/js/components/cardList.js
@@ -7,9 +7,13 @@ function CardList(data) {
   this.cardListElem.classList.add("card-list");
   this.cardListElem.setAttribute("data-count", 0);
   this.cardListElem.setAttribute("id", data.id);
-
-  // DOM에 반영
-  document.querySelector(".carousel:last-child").appendChild(this.cardListElem);
+  
+  
+  // DOM에 반영: document.querySelector(".carousel:last-child").appendChild(this.cardListElem);
+  data.parentNode.appendChild(this.cardListElem);
+  
+  // parentNode 속성 변경
+  data.parentNode = this.cardListElem;
   
   // Card 컴포넌트 생성
   this.cardCreate(data);
@@ -17,11 +21,12 @@ function CardList(data) {
 
 CardList.prototype = {
   prototype: CardList,
-  cardCreate: function ({ images, width, height }) {
+  cardCreate: function ({ images, width, height, parentNode }) {
     images.forEach(({ download_url }, index) => {
       const data = {
         width,
         height,
+        parentNode,
         index: index,
         imageUrl: download_url,
       };
--- carousel-slider/js/components/carousel.js
+++ carousel-slider/js/components/carousel.js
@@ -9,11 +9,16 @@ function Carousel(data) {
   this.carouselElem.style.height = data.height;
   
   // DOM에 반영
-  document.querySelector(".container").appendChild(this.carouselElem);
+  // document.querySelector(".container").appendChild(this.carouselElem);
+  data.parentNode.appendChild(this.carouselElem);
 
+  // 부모 노드 속성 바꿔주기
+  data.parentNode = this.carouselElem
   // CardList 컴포넌트 생성 
   new CardList(data);
 
+  // 부모 노드 속성 바꿔주기
+  data.parentNode = this.carouselElem
   // Button 컴포넌트 생성
   new Button(data);
 }
